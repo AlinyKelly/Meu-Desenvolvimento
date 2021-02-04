b5778e54a7e2b1d46be0ac9ee95559e5fd23c24b
Aliny Kelly <alinykellyfs@gmail.com>
2021-02-04 20:38:30 -0300

    Conteudo da aula parte 1

---
+++ Digital_Innovation_One/Bootcamp_HTML_Web_Developer/Intro_JavaScript/README.md
@@ -0,0 +1,23 @@
+# Curso Programação para Internet com JavaScript
+Neste curso introdutório apredemos a trabalhar com JavaScript.
+Bootcamp: HTML Web Developer
+Professor: Rafael Galleani
+
+### Roteiro da Aula
+Alert
+Variáveis
+Console.log
+Array e dicionário
+Condicional (id else)
+Laços de repetição (while e for)
+Push, pop e length
+Reverse Array
+toString para array
+join para array
+Variável Global / Local
+Replace
+lowerCase, upperCase
+Redirecionar para outra página
+date
+function
+
---
+++ Digital_Innovation_One/Bootcamp_HTML_Web_Developer/Intro_JavaScript/js/main.js
@@ -0,0 +1,58 @@
+
+//----------------AULA 1-------------------
+
+//var nome = "Aliny Kelly";
+//var idade = 31;
+//var idade2 = 10;
+//var n1 = 5;
+//var n2 = 15;
+//var frase = "Japão é o melhor time do mundo."
+// alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLocaleLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+
+//-----------------AULA 2--------------------
+
+// ---------------- Array -------------------
+/*
+var lista = ["Maçã", "Pêra", "Uva"];
+lista.push("Laranja"); //Inserir um elemento
+lista.pop(); //Retira um elemento
+console.log(lista[1]); //Exibi apenas um nome
+console.log(lista); //Exibi a lista
+console.log(lista.length); //Exibi a quantidade de elementos na lista
+console.log(lista.reverse()); //Exibi os elementos de trás para frente
+console.log(lista.toString());
+console.log(lista.join(" - ")); //Muda a forma como os elementos são mostrados
+*/
+
+//-------------------Dicionário------------------
+//Exemplo 1
+/*
+var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+//Exemplo 2 - Lista de Dicionário
+var frutas = [{nome: "maçã", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log(fruta);
+alert(frutas[1].nome);
+*/
+
+//-----------------AULA 3--------------------
+
+/*var idade = prompt("Qual a sua idade?");
+if (idade >=18){
+    alert("Maior de idade");
+}else {
+    alert("Menor de idade.")
+}
+*/
+var count = 5;
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+}

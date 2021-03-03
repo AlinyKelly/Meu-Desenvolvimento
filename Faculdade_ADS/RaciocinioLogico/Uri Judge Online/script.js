09269544c226ed2c42c40c49e5f9f93eacf0e579
Aliny Kelly <alinykellyfs@gmail.com>
2021-03-03 13:54:42 -0300

    Resolucao

--- Faculdade_ADS/RaciocinioLogico/Uri Judge Online/script.js
+++ Faculdade_ADS/RaciocinioLogico/Uri Judge Online/script.js
@@ -1,8 +1,10 @@
 var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split('\n');
 
-var [N1, N2, N3, N4]= input.split(" ").map(item => parseFloat(item));
+const [N1, N2, N3, N4] = lines.shift().split(" ");
+const [notaExame] = lines.shift().split(" ");
 
-var media = ((N1*2) + (N2*3) + (N3*4) + (N4*1)) / 10;
+const media = ((N1*2) + (N2*3) + (N3*4) + (N4*1)) / 10;
 console.log("Media: " + media.toFixed(1));
 
 if (media >= 7.0){
@@ -11,17 +13,17 @@ if (media >= 7.0){
 } else if (media >= 5.0 && media <= 6.9){
     console.log("Aluno em exame.");
 
-    console.log("Nota do exame: " + N5);
+    console.log("Nota do exame: " + notaExame.toFixed(1));
 
-    media = (media + N5) / 2;
+        media = (media + notaExame) / 2;
 
-    if (media >= 5.0){
-        console.log("Aluno aprovado.");
-    } else {
-        console.log("Aluno reprovado.");
-    }
+        if (media >= 5.0){
+            console.log("Aluno aprovado.");
+        } else {
+            console.log("Aluno reprovado.");
+        }
 
-    console.log("Media final: " + media.toFixed(1));
+        console.log("Media final: " + media.toFixed(1));
     
 } else {
     console.log("Aluno reprovado.");
